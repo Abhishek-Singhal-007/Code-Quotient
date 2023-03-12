a867ef0fda3d6f457036399f11ccefbe8df0b62d
Abhishek Singhal <119617227+Abhishek-Singhal-007@users.noreply.github.com>
2023-03-12 21:18:05 +0530

    Update 10.Arithmetic_Operations.java

--- Warm up/10.Arithmetic_Operations.java
+++ Warm up/10.Arithmetic_Operations.java
@@ -1,1 +1,27 @@
-
+import java.util.Scanner;
+// Other imports go here
+// Do NOT change the class name
+class Main{
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int ans = 0;
+      char symbol = sc.next().charAt(0);
+      if(symbol == '+')
+        ans = a + b;
+      else if(symbol == '-')
+        ans = a - b;
+      else if(symbol == '%')
+        ans = a % b;
+      else if(symbol == '/')
+        ans = a / b;
+      else if(symbol == '*')
+        ans = a * b;
+      else
+        System.out.println("Enter valid arithmetic operator");
+      if(ans != 0)
+      	System.out.println(ans);
+    }
+}
